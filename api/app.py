e8b88f20a8823ade3d8964da1496e0136b48d948
Constantinescu Vlad <vlad.constantinescu@xfactorapp.com>
2017-12-09 20:30:05 +0200

    added login and register api endpoints

--- api/app.py
+++ api/app.py
@@ -1,13 +1,40 @@
 #!flask/bin/python
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+client = MongoClient('localhost:27017')
+db = client.sac
 
-@app.route('/api/test', methods=['GET'])
-def index():
-    return jsonify({'test': 'test'})
+
+@app.route('/api/register', methods=['POST'])
+def register():
+    table = db.users
+    user_id = table.insert({
+        "name": request.json['name'],
+        "email": request.json['email'],
+        "password": request.json['password']
+    })
+
+    user = table.find_one({'_id': ObjectId(str(user_id))})
+    user['_id'] = str(user['_id'])
+
+    return jsonify({"success": True, "user": user})
+
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    user = db.users.find({
+        'email': request.json['email'],
+        'password': request.json['password']
+    })
+
+    if len(user) == 0:
+        return jsonify({'success': False, 'error': 'Invalid credentials'})
+
+    return jsonify({'success': True, 'user': user})
 
 
 if __name__ == '__main__':
